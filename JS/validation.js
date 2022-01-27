14edee212bc86d9bed408c138d8da0027a8d5031
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-01-27 08:44:56 +0000

    Add lowercase validation to the email

--- JS/validation.js
+++ JS/validation.js
@@ -13,3 +13,22 @@ alertMsg.style.fontWeight = '600';
 alertMsg.style.textShadow = '1px 1px 5px #60C095';
 alertMsg.style.padding = '10px';
 alertMsg.style.visibility = 'hidden';
+
+form.addEventListener('submit', (e) => {
+    if (email.value !== email.value.toLowerCase()) {
+      alertMsg.style.visibility = 'visible';
+      email.style.border = ' 1px solid #60C095';
+      email.style.boxShadow = '1px 1px 10px #008552';
+  
+      setTimeout(() => {
+        alertMsg.style.visibility = 'hidden';
+        email.style.border = '1px solid #d0d9d4';
+        email.style.boxShadow = 'none';
+      }, 8000);
+      e.preventDefault();
+    } else {
+      alertMsg.style.visibility = 'hidden';
+      email.style.border = '1px solid #d0d9d4';
+    }
+  });
+  
