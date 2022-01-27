6fcc8be3adb9d45cb0648403721dfa46273efd57
Cynthia Inga <candyinga67@gmail.com>
2022-01-27 11:46:52 +0200

    Fix linters errors

--- JS/validation.js
+++ JS/validation.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form');
-const nameInput = document.getElementById('name');
+// const nameInput = document.getElementById('name');
 const email = document.getElementById('mail');
-const messageInput = document.getElementById('msg');
+// const messageInput = document.getElementById('msg');
 
 const alertParent = document.querySelector('.btn-submit-alert');
 const alertMsg = document.createElement('p');
@@ -15,20 +15,19 @@ alertMsg.style.padding = '10px';
 alertMsg.style.visibility = 'hidden';
 
 form.addEventListener('submit', (e) => {
-    if (email.value !== email.value.toLowerCase()) {
-      alertMsg.style.visibility = 'visible';
-      email.style.border = ' 1px solid #60C095';
-      email.style.boxShadow = '1px 1px 10px #008552';
-  
-      setTimeout(() => {
-        alertMsg.style.visibility = 'hidden';
-        email.style.border = '1px solid #d0d9d4';
-        email.style.boxShadow = 'none';
-      }, 8000);
-      e.preventDefault();
-    } else {
+  if (email.value !== email.value.toLowerCase()) {
+    alertMsg.style.visibility = 'visible';
+    email.style.border = ' 1px solid #60C095';
+    email.style.boxShadow = '1px 1px 10px #008552';
+
+    setTimeout(() => {
       alertMsg.style.visibility = 'hidden';
       email.style.border = '1px solid #d0d9d4';
-    }
-  });
-  
+      email.style.boxShadow = 'none';
+    }, 8000);
+    e.preventDefault();
+  } else {
+    alertMsg.style.visibility = 'hidden';
+    email.style.border = '1px solid #d0d9d4';
+  }
+});
