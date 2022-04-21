4cfea773fe2e9f2b87b520d9187f1ced8423cc26
Cynthia Inga <candyinga67@gmail.com>
2022-04-21 12:32:41 +0200

    Add a project to my portfolio

--- JS/worksection.js
+++ JS/worksection.js
@@ -67,35 +67,34 @@ const projects = [
   {
     id: 'project_3',
     images: {
-      img: './image/Snapshoot-Portfolio(1).png',
+      img: './image/continents.png',
       cancelPop: './image/icons/Disabled.svg',
       altText: 'project 3 preview image',
     },
-    name: 'Multi-Post Stories Gain+Glory',
-    heading: 'Keeping track of hundreds of components',
-    languages: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+    name: 'Covid-19 Tracker',
+    heading: 'COVID-19 TRACKER',
+    languages: ['React & Redux', 'JavaScript', 'HTML', 'CSS'],
     technologies: [
       'Codekit',
       'GitHub',
       'Javascript',
-      'Bootstrap',
       'Terminal',
       'Codepen',
     ],
 
     button: 'See Project',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      'Covid-19-Webapp is a tracking site that provides the necessary information on covid-19 cases in any African country and other continents their update.',
     list: [
       {
         text: 'See Live',
         icon: './image/icons/Icon-Export@2x.svg',
-        liveDemo: 'https://cynthiainga.github.io/',
+        liveDemo: 'https://covid-19-appli.netlify.app/',
       },
       {
         text2: 'See Source',
         icon: './image/icons/Icon-GitHub-1.svg',
-        codeSource: 'https://github.com/cynthiainga/Personal-Portfolio',
+        codeSource: 'https://github.com/cynthiainga/Covid-19-webapp',
       },
     ],
   },
